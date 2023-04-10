cd139a4237b8e96303bfef4f7924e335f3de254c
Muhamad Nur Asyraaf <masyraaf14@gmail.com>
2023-04-11 00:49:37 +0800

    another commit

--- Home.java
+++ Home.java
@@ -1,7 +1,13 @@
+import java.util.LinkedList;
+
 class Home{
     public static void main(String[] args) {
+
+        LinkedList llist = new LinkedList();
         for(int i = 0;i<100;i++){
             System.out.println("Corntall");
         }
     }
+
+
 }
